ed002b86bd9f9ebf3bbbae047b298b4567002011
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-09-25 21:15:48 +0800

    RiseForRoblox

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -625,15 +625,22 @@ for i, v in pairs({"Search", "Combat", "Movement", "Player", "Render", "Exploit"
                 task.wait(.1)
                 buttonobj.BackgroundColor3 = Color3.fromRGB(18, 21, 27)
             end)
-            local expandsize = 0
-            expandsize = ((#options:GetChildren() - 1) * 15) + ((#options:GetChildren() - 2) * 12)
-            if expandsize == 0 then return end -- no settings, no tween shit
-            if options.Size == UDim2.new(1, 0, 0, 0) then
-                buttonobj:TweenSize(UDim2.new(0, 566, 0, 85 + expandsize), nil, nil, 0.1)
-                options:TweenSize(UDim2.new(1, -26, 0, expandsize), nil, nil, 0.1)
-            else
-                buttonobj:TweenSize(UDim2.new(0, 566, 0, 75), nil, nil, 0.1)
-                options:TweenSize(UDim2.new(1, -26, 0, 0), nil, nil, 0.1)
+            options.AutomaticSize = options.AutomaticSize == Enum.AutomaticSize.None and Enum.AutomaticSize.Y or Enum.AutomaticSize.None
+            buttonobj:TweenSize(UDim2.new(0, 566, 0, (options.AutomaticSize == Enum.AutomaticSize.None and 0 or (85 + (
+                (#options:GetChildren() - 1) * 15 + ((#options:GetChildren() - 2) * 12)
+            )))), nil, nil, 0.1)
+            for i2, v2 in pairs(options:GetDescendants()) do
+                local prop = ""
+                if v2:IsA("TextLabel") or v2:IsA("TextButton") or v2:IsA("TextBox") then
+                    prop = "TextTransparency"
+                elseif v2:IsA("Frame") then
+                    prop = "BackgroundTransparency"
+                end
+                if v2:HasTag("NoTween") then return end
+                v2[prop] = 1
+                tweenService:Create(v2, TweenInfo.new(0.1), {
+                    [prop] = 0
+                }):Play()
             end
         end)
 
