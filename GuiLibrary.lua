69743a812e44f45bed5241ad4f8004010105aabc
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-09-29 11:26:27 +0800

    RiseForRoblox

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -390,7 +390,7 @@ local function colortick()
         if v == nil then
             return
         end
-        local indexval = ((v:IsA("UIGradient") and v.Parent or v).AbsolutePosition.Y / DisplayY) * 0.005
+        local indexval = ((v:IsA("UIGradient") and v.Parent or v).AbsolutePosition.Y / DisplayY) * 0.015
         local realstep = defaultyprogress + indexval
         if realstep > 1 then
             realstep = 1 - (realstep - 1)
@@ -410,7 +410,7 @@ local function colortick()
         if v == nil or GuiLibrary.Settings.Theme ~= "Rainbow" then
             return
         end
-        local indexval = ((v:IsA("UIGradient") and v.Parent or v).AbsolutePosition.Y / DisplayY) * 0.005
+        local indexval = ((v:IsA("UIGradient") and v.Parent or v).AbsolutePosition.Y / DisplayY) * 0.015
         local realstep = defaultyprogress + indexval
         if realstep > 1 then
             realstep = 1 - realstep
