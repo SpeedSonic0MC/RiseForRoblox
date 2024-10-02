85fbe87b1b866faac3e92094460f2432951d39ca
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-10-02 20:59:46 +0800

    RiseForRoblox

--- services/ColorService.lua
+++ services/ColorService.lua
@@ -177,7 +177,8 @@ Themes["getAccentColor"] = function(theme, screenCoordinates)
         end
         return Themes.mixColors(th[1], th[2], Themes.getBlendFactor(screenCoordinates))
     else
-        local rainbowstate = math.ceil((DateTime.now().UnixTimestampMillis + (screenCoordinates.Y * 20)) / 10)
+        local rainbowstate = math.ceil(DateTime.now().UnixTimestampMillis + (screenCoordinates.X * 0.005 + screenCoordinates.Y *
+                        0.06 * 10))
         rainbowstate = rainbowstate % 360
         return Color3.fromHSV(rainbowstate / 360.0, 0.6, 1)
     end
