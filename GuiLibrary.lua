cbadc13e251b6953ab324047deb67184bd81f9f4
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-10-03 19:16:20 +0800

    RiseForRoblox

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -575,30 +575,40 @@ local initWindowFunction = {
                 themex.ImageTransparency = 1
                 local xuigra = Instance.new("UIGradient", themex)
                 if theme ~= "Rainbow" then
-                    local colorseq = GuiLibrary:GetColorSequence(theme)
-                    local tv = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
-                    local create1 = tweenService:Create(xuigra, tv, {Offset=Vector2.new(-1,0)})
-                    local startpos = Vector2.new(1, 0)
-                    local rot = 180
-                    xuigra.Offset = startpos
-                    xuigra.Rotation = 0
-                    xuigra.Color = colorseq
-                    create1:Play()
-                    local function comp()
-                        if rot == 0 then
-                            xuigra.Rotation = 180
-                            xuigra.Offset = startpos
-                            create1:Play()
-                        elseif rot == 180 then
-                            xuigra.Rotation = 0
-                            xuigra.Offset = startpos
-                            create1:Play()
+                    if #ThemeService.Themes[theme] == 2 then
+                        local colorseq = GuiLibrary:GetColorSequence(theme)
+                        local tv = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+                        local create1 = tweenService:Create(xuigra, tv, {
+                            Offset = Vector2.new(-1, 0)
+                        })
+                        local startpos = Vector2.new(1, 0)
+                        local rot = 180
+                        xuigra.Offset = startpos
+                        xuigra.Rotation = 0
+                        xuigra.Color = colorseq
+                        create1:Play()
+                        local function comp()
+                            if rot == 0 then
+                                xuigra.Rotation = 180
+                                xuigra.Offset = startpos
+                                create1:Play()
+                            elseif rot == 180 then
+                                xuigra.Rotation = 0
+                                xuigra.Offset = startpos
+                                create1:Play()
+                            end
                         end
+                        create1.Completed:Connect(function()
+                            if rot == 0 then
+                                rot = 180
+                            elseif rot == 180 then
+                                rot = 0
+                            end
+                            comp()
+                        end)
+                    elseif #ThemeService.Themes[theme] == 1 then
+                        xuigra.Color = ColorSequence.new(ThemeService.Themes[theme][1])
                     end
-                    create1.Completed:Connect(function()
-                        if rot == 0 then rot = 180 elseif rot == 180 then rot = 0 end
-                        comp()
-                    end)
                 end
                 local text = Instance.new("TextLabel", themex)
                 text.BackgroundTransparency = 1
