2f6766a1dc3d0ab35bc3f91c0c94ac408cada6a8
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-10-03 19:20:59 +0800

    RiseForRoblox

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -577,7 +577,7 @@ local initWindowFunction = {
                 if theme ~= "Rainbow" then
                     if #ThemeService.Themes[theme] == 2 then
                         local colorseq = ThemeService:GetColorSequence(theme)
-                        local tv = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+                        local tv = TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
                         local create1 = tweenService:Create(xuigra, tv, {
                             Offset = Vector2.new(-1, 0)
                         })
