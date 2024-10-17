0a5362ae6833c0761ccb2f7eb0bab3f1c46020f1
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-10-17 22:43:40 +0800

    RiseForRoblox

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,3 @@
-if not game.Loaded then
-    game.Loaded:Wait()
-end
 local function loadscript(url, e)
     if shared.RiseDeveloper then
         if isfile("rise/" .. url) then
@@ -8,7 +5,7 @@ local function loadscript(url, e)
         end
     end
     local suc, res = pcall(function()
-        return game:HttpGet("https://raw.githubusercontent.com/SpeedSonic0MC/RiseForRoblox/main/" .. url, true)
+        return game:HttpGet("https://raw.githubusercontent.com/SpeedSonic0MC/RiseForRoblox/main/" .. url)
     end)
     if not suc or res == "404: Not Found" then
         print("Rise >> Failed to execute loadscript()")
@@ -18,11 +15,13 @@ end
 assert(not shared.VapeExecuted, "Rise >> Rise is incompatible with VapeV4ForRoblox.")
 assert(not shared.RiseExecuted, "Rise >> Rise 6 already injected")
 shared.RiseExecuted = true
-for i, v in pairs({"rise", "rise/assets", "rise/configs", "rise/scripts"}) do
-    if not isfolder(v) then
-        makefolder(v)
+task.spawn(function()
+    for i, v in pairs({"rise", "rise/assets", "rise/configs", "rise/scripts"}) do
+        if not isfolder(v) then
+            makefolder(v)
+        end
     end
-end
+end)
 local oldtick = tick()
 shared.Rise = loadstring(loadscript("RiseService.lua"))()
 local scr = loadscript("GuiLibrary.lua")
@@ -35,7 +34,8 @@ if not GuiLibrary["Loaded"] then
     until GuiLibrary["Loaded"] == true
 end
 local suc, res = pcall(function()
-    return game:HttpGet("https://raw.githubusercontent.com/SpeedSonic0MC/RiseForRoblox/main/game/" .. game.PlaceId .. ".lua", true)
+    return game:HttpGet("https://raw.githubusercontent.com/SpeedSonic0MC/RiseForRoblox/main/game/" .. game.PlaceId ..
+                            ".lua", true)
 end)
 if not suc or res == "404: Not Found" then
     print("Rise >> Rise is incompatible with this game currently. Be back soon!")
