9f147a542fbdbe45b0e3e3d33a1476fe61ddbe49
SpeedSonic0MC <speedsonic0mc@gmail.com>
2024-12-22 10:30:51 +0800

    GitPush

--- Modules/189707.lua
+++ Modules/189707.lua
@@ -1,2 +1,13 @@
 --[[ Game: Natural Disaster Survival ]]
 local GuiLibrary = shared.Rise.GuiLibrary
+
+local fling = GuiLibrary.ObjectsThatCanBeSaved.ExploitWindow.CreateModule({
+    Name = "189707fling",
+    Function = function(value)
+    end
+})
+
+local flingMode = fling.CreateModeValue({
+    Value = 1,
+    Options = {"Normal", "Fly", "Walk"}
+})
