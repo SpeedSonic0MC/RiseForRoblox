3585c684649488e3b43cc776949483d411511f70
SpeedSonic0MC <speedsonic0mc@gmail.com>
2025-01-03 19:41:06 +0800

    GitPush

---
+++ Modules/4079902982.lua
@@ -0,0 +1,20 @@
+shared.Rise.CustomSave = 1537690962
+local geturl = function(p)
+    local customurl = "https://raw.githubusercontent.com/SpeedSonic0MC/RiseForRoblox/main/" .. p
+    if shared.RiseDeveloper and shared.RiseUrls[p] then
+        customurl = shared.RiseUrls[p]
+    end
+    local suc, res = pcall(function()
+        return game:HttpGet(customurl)
+    end)
+    if not suc or res == "404: Not Found" then
+        error("❌ Failed to load rise file : rise/" .. p)
+    end
+    return res
+end
+local sx, fx = pcall(function()
+    return geturl("Modules/1537690962.lua")
+end)
+if sx then
+    loadstring(fx)()
+end
